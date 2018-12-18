da156328343a278a694846fa6ae394f19fcd1c2b
Chris Athanas <realityexpanderdev@gmail.com>
2018-12-18 00:19:34 -0600

    Create arrayBasics.js

---
+++ arrayBasics.js
@@ -0,0 +1,23 @@
+function basicAccessing(){
+  //For this function, make answer be equal to the second item in the array.
+  var myArray = ["alpha","beta","charlie", "delta"]
+  var answer = myArray[1]
+  
+  return answer
+}
+
+function basicAccessing2(){
+  //For this function, make answer be equal to the third item in the array.
+  var myArray = [1,2,3,4]
+  var answer = myArray[2]
+  
+  return answer
+}
+
+function nestedArrays(){
+  //For this function, make answer be equal to the second item in the third nested array.
+  var myArray = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+  var answer = myArray[2][1]
+  
+  return answer
+}
